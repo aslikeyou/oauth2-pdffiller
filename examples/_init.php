3feb189275d7508e60665cc4e36be2bc4708925d
Serghei Iakovlev <sergeyklay@users.noreply.github.com>
2015-11-27 14:40:29 +0200

    Update _init.php

--- examples/_init.php
+++ examples/_init.php
@@ -1,29 +1,34 @@
 <?php
 
+use aslikeyou\OAuth2\Client\Provider\Pdffiller;
+use Flintstone\Flintstone;
+use AdammBalogh\KeyValueStore\Adapter\FileAdapter;
+use AdammBalogh\KeyValueStore\KeyValueStore;
+use Carbon\Carbon;
+
 require __DIR__.'/../vendor/autoload.php';
 require __DIR__.'/../src/Pdffiller.php';
 require __DIR__.'/../src/BaseEntity.php';
 
-$provider = new \aslikeyou\OAuth2\Client\Provider\Pdffiller([
-    'clientId'                => '9ae2dd4788374224',
-    'clientSecret'            => 'aR7UlwmJyaxQeJruo1eHyLXzemf3tNB8',
-    'urlAccessToken'          => 'http://api.pdffiller.local/v1/oauth/access_token',
-    'urlApiDomain'            => 'http://api.pdffiller.local/v1/'
+$provider = new Pdffiller([
+    'clientId'       => '9ae2dd4788374224',
+    'clientSecret'   => 'aR7UlwmJyaxQeJruo1eHyLXzemf3tNB8',
+    'urlAccessToken' => 'http://api.pdffiller.local/v1/oauth/access_token',
+    'urlApiDomain'   => 'http://api.pdffiller.local/v1/'
 ]);
 
 $tz = 'Europe/Kiev';
 
-$fileClient = Flintstone\Flintstone::load('usersDatabase', ['dir' => '/tmp']);
-$adapter = new AdammBalogh\KeyValueStore\Adapter\FileAdapter($fileClient);
-$kvs = new AdammBalogh\KeyValueStore\KeyValueStore($adapter);
+$kvs = new KeyValueStore(new FileAdapter(Flintstone::load('usersDatabase', ['dir' => '/tmp'])));
 
 $accessTokenKey = 'access_token';
 
-if($kvs->has($accessTokenKey)) {
-    $accessTokenString = $kvs->get($accessTokenKey);
-} else {
+if (!$accessTokenString = $kvs->has($accessTokenKey)) {
     $accessToken = $provider->getAccessToken();
-    $liveTimeInSec = \Carbon\Carbon::createFromTimestamp($accessToken->getExpires(), $tz)->diffInSeconds(\Carbon\Carbon::now($tz));
+    $liveTimeInSec = Carbon::createFromTimestamp(
+        $accessToken->getExpires(),
+        $tz
+    )->diffInSeconds(Carbon::now($tz));
     $kvs->set('access_token', $accessToken->getToken());
     $kvs->expire('access_token', $liveTimeInSec);
     $accessTokenString = $accessToken->getToken();
